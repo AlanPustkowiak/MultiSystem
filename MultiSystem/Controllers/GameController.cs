93b15c77226b25bd1e7387703d11d13851dee9ea
AlanPustkowiak <alan.pustkowiak@outlook.com>
2024-10-08 21:18:48 +0200

    CRUD ready

--- MultiSystem/Controllers/GameController.cs
+++ MultiSystem/Controllers/GameController.cs
@@ -22,14 +22,33 @@ namespace MultiSystem.Controllers
             var game = await _gameService.GetGameById(Id);
             return Ok(game);
         }
-        //[HttpGet]
-        //public async Task<ActionResult<Game>>
+
+        [HttpGet("all")]
+        public async Task<ActionResult<List<Game>>> GetAllGames()
+        {
+            var games = await _gameService.GetAllGames();
+            return games;
+        }
 
         [HttpPost]
         public async Task<ActionResult<Game>> AddGame(Game game)
         {
             var addedGame = await _gameService.AddGame(game);
             return Ok(addedGame);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<ActionResult<Game>> EditGame(int Id, Game game)
+        {
+            var editedGame = await _gameService.EditGame(Id, game);
+            return Ok(editedGame);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<bool>> DeleteGame(int Id)
+        {
+            var result = await _gameService.DeleteGame(Id);
+            return Ok(result);
         }
     }
 }
